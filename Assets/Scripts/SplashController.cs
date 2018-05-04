165e82b766ac9c71d3f4a30ca92b848f2fa9edd5
Tim McLennan <tim@nothingness.ca>
2018-05-04 18:23:32 -0400

    add basic splash/game scenes and a couple scripts to control them.

---
+++ Assets/Scripts/SplashController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplashController : MonoBehaviour
+{
+	private void Update()
+	{
+		if (Input.anyKeyDown) {
+			UnityEngine.SceneManagement.SceneManager.LoadScene("Game", UnityEngine.SceneManagement.LoadSceneMode.Single);
+		}
+	}
+}
---
+++ Assets/Scripts/StaticData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LitJson;
+
+public class StaticData : MonoBehaviour
+{
+	public static StaticData Instance {
+		get;
+		private set;
+	}
+}
