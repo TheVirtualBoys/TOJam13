a1d709de1926ebfed2d9e5e783301c184585fa6c
David Fono <fono@mobilefono.com>
2018-05-05 15:10:52 -0400

    boilerplate functionality to click on points/buttons and generate menus with more buttons

---
+++ Assets/Scripts/UI/InGameUI/InteractionButton.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class InteractionButton : MonoBehaviour {
+
+    public InteractionMenu menuPrefab;
+
+    private NodeData node;
+
+    /// <summary>
+    /// The button that'll handle the interaction with this interaction point.
+    /// </summary>
+    [SerializeField]
+    private Button interactionPointButton = null;
+
+    public void Init(NodeData node) {
+        this.node = node;
+    }
+
+    #region Monobehaviour
+
+    /// <summary>
+    /// Get the buttons.
+    /// </summary>
+    private void Reset() {
+        this.interactionPointButton = this.GetComponent<Button>();
+    }
+
+    /// <summary>
+    /// Start this instance.
+    /// </summary>
+    private void Start() {
+        this.interactionPointButton.onClick.AddListener(this.HandleButtonPressed);
+    }
+
+    /// <summary>
+    /// Ons the destroy.
+    /// </summary>
+    private void OnDestroy() {
+        this.interactionPointButton.onClick.RemoveListener(this.HandleButtonPressed);
+    }
+
+    #endregion
+
+    /// <summary>
+    /// Handles the button being pressed.
+    /// </summary>
+    private void HandleButtonPressed() {
+        NodeData node = DataManager.Instance.rootNode.GetChild(this.node.id);
+        InteractionMenu menu = GameObject.Instantiate(this.menuPrefab);
+        menu.Init(AdventureLog.Instance.FilterAvailableNodes(node.children));
+    }
+
+}
