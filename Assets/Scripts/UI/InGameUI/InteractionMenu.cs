33ad2f8049275d7b6623154c25f7cf660b8b0259
Kris <kris@xmgstudio.com>
2018-05-06 16:45:13 -0400

    New item stuff

--- Assets/Scripts/UI/InGameUI/InteractionMenu.cs
+++ Assets/Scripts/UI/InGameUI/InteractionMenu.cs
@@ -8,16 +8,26 @@ public class InteractionMenu : MonoBehaviour {
 
     public Transform buttonContainer;
 
+    private List<InteractionButton> buttons = new List<InteractionButton>();
+
     public void Init(List<NodeData> nodes) {
+        this.Clear();
         foreach (NodeData node in nodes) {
             InteractionButton button = GameObject.Instantiate(this.buttonPrefab);
             button.Init(node, this);
             button.transform.SetParent(this.buttonContainer);
+            this.buttons.Add(button);
         }
     }
 
     public void Close() {
         GameObject.Destroy(this.gameObject);
+    }
+
+    public void Clear() {
+        foreach(InteractionButton button in this.buttons) {
+            GameObject.Destroy(button.gameObject);
+        }
     }
 
 }
